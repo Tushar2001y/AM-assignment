19f415e25b263023efe0b201242fbeadec18efd1
tushar chaudhary <63543130+Tushar2001y@users.noreply.github.com>
2022-07-21 00:57:15 +0530

    Delete rmgx directory

--- rmgx/src/main/java/com/project/asset/rmgx/repositories/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.project.asset.rmgx.repositories;
-
-import com.project.asset.rmgx.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-}
